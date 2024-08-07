a7437a4c511cae0fd87e4d9d67f2d66f65e01e2a
Pablo Dobry <pablo.dob@gmail.com>
2024-08-07 16:41:26 -0300

    Agregado de modelo reunion usuario y getuser en usuario

---
+++ Negocio/ReunionUsuarioNegocio.cs
@@ -0,0 +1,39 @@
+﻿using Entidades;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class ReunionUsuarioNegocio
+    {
+        static readonly string defaultURL = "http://localhost:5275/api/ReunionUsuario/";
+        public async static Task<IEnumerable<ReunionUsuario>> GetAll()
+        {
+            var response = await Conexion.Instancia.Cliente.GetStringAsync(defaultURL);
+            var data = JsonConvert.DeserializeObject<List<ReunionUsuario>>(response);
+            return data;
+        }
+
+        public async static Task<Boolean> Delete(ReunionUsuario reunionusuario)
+        {
+            var response = await Conexion.Instancia.Cliente.DeleteAsync(defaultURL + reunionusuario.Id);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async static Task<Boolean> Add(ReunionUsuario reunionusuario)
+        {
+            var response = await Conexion.Instancia.Cliente.PostAsJsonAsync(defaultURL, reunionusuario);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async static Task<Boolean> Update(ReunionUsuario reunionusuario)
+        {
+            var response = await Conexion.Instancia.Cliente.PutAsJsonAsync(defaultURL + reunionusuario.Id, reunionusuario);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
